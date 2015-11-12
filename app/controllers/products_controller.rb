0c34d1347761a4a300e0a5fe6213a71291d71ac8
Jamie <gatorjuice@gmail.com>
2015-11-11 20:53:40 -0600

    created columns

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,22 @@ class ProductsController < ApplicationController
 
   def index 
     @products = Product.all
+    @products1 = []
+    @products2 = []
+    @products3 = []
+    @products.each_with_index do |product, index| 
+      if index%3 == 0
+        @products1 << product 
+      elsif index%3 == 1
+        @products2 << product 
+      elsif index%3 == 2
+        @products3 << product
+      end
+    end
+  end
+
+  def view
+    @product = Product.find_by(name: "Giant Rubber Rat")
   end
 
 end
